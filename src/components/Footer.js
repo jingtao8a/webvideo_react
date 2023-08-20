d49fea1ab72373d15d3b46b1fe41c00828a1fc6c
yuxintao <1921056015@qq.com>
2023-08-20 16:36:14 +0800

    完成Footer部分

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React  from "react";
-
+import Logo from "../img/logo.png";
 const Footer = ()=> {
-    return <div className="footer"></div>
+    return (
+    <div className="footer">
+        <img src={Logo} alt="" />
+        <span>
+            Made with ♥️ and <b>React.js</b>.
+        </span>
+    </div>);
 };
 
 export default Footer;
