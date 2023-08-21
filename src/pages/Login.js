dded768cf8c6fcc94f02c6e063e117a8129c3fea
yuxintao <1921056015@qq.com>
2023-08-21 15:16:35 +0800

    Login成功和后端对接

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,44 @@
-import React  from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"
 
 const Login = ()=> {
+    const [inputs, setInputs] = useState({
+        userName:"", 
+        password:"",
+    });
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    console.log(inputs);
+    const handleChange = (event)=> {
+        setInputs(prevInputs=> {
+            return {...prevInputs, [event.target.name] : event.target.value};
+        });
+    };
+
+    const handleSubmit = async (event)=> {
+        event.preventDefault();
+        try {
+            let res = await axios.post("/auth/login", inputs);
+            console.log(res);
+            if (res.data.code !== 200) {
+                setError(res.data.message);
+            } else {
+                navigate("/");
+            }
+        } catch (err) {
+            setError(err);
+        }
+    };
     return (
     <div className="auth">
         <h1>Login</h1>
         <form>
-            <input type='text' placeholder="username"></input>
-            <input type='password' placeholder="password"></input>
-            <button>Login</button>
+            <input type='text' placeholder="username" name="userName" onChange={handleChange}></input>
+            <input type='password' placeholder="password" name="password" onChange={handleChange}></input>
+            <button onClick={handleSubmit}>Login</button>
+            {error && <p>{error}</p>}
             <span>Don't you have an account? <Link to="/register">Register</Link></span>
         </form>
     </div>);
