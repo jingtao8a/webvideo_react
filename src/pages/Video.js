8fb03defee5d5ddc920d1a0406ec3153772b0067
yuxintao <1921056015@qq.com>
2023-08-25 22:10:11 +0800

    引入video.js用于播放m3u8视频

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,7 +1,21 @@
+import axios from 'axios';
+import React, {useState, useEffect, useRef} from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 const Video = ()=> {
-    return <div className="video">
-        video
-    </div>
+  const path = "/video?videoPath=VID_20230211_151132.mp4"
+  // const path = "/video?videoPath=dir2/VID_20230123_164414.mp4";
+    // const path = "/video?videoPath=dir1/VID_20230109_132811.mp4";
+    // const path = "/video/dir1/VID_20230109_132811/VID_20230109_132811.m3u8";
+    return (
+        <div className="video">
+            <video id="myVideo" class="video-js vjs-default-skin vjs-big-play-centered" muted controls autoplay  preload="auto" width="500" height="400" data-setup='{}'>      
+                <source id="source" src={path}  type="application/x-mpegURL"/>    
+            </video> 
+
+        </div>);
+    
 };
+
 
 export default Video;
