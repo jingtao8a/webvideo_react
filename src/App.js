5118a1783733998138cbd2347c9e316ce452f9bc
yuxintao <1921056015@qq.com>
2023-08-27 10:32:06 +0800

    使用HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
   // Route,
   Outlet,
@@ -21,7 +21,7 @@ const Layout = ()=> {
     );
 };
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/", 
     element: <Layout/>,
